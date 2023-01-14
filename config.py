564b448dc9f6aa8b26b918977f6663b3ed7b49a8
FILA7 <98689054+FILA7gh@users.noreply.github.com>
2023-01-14 19:11:58 +0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher, Bot
-from decouple import config
-
-bot = Bot(config('TOKEN'))  # привязка токена
-dp = Dispatcher(bot=bot)
-
-ADMINS = (2081434201, 819212459)  # айди админов
