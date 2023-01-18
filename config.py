12374a60a995de654aba4d5200ec8313302056fa
FILA7 <98689054+FILA7gh@users.noreply.github.com>
2023-01-18 20:12:43 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher, Bot
-from decouple import config
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-storage = MemoryStorage()  # создаем кэш
-bot = Bot(config('TOKEN'))  # привязка токена
-dp = Dispatcher(bot=bot, storage=storage)
-
-ADMINS = (2081434201, 819212459)  # айди админов
